a7aeea57906e8b3fa81ec0bf1c3ce36ae7623ac1
YannGda <yann.gayda@gmail.com>
2022-02-21 13:24:39 +0100

    global info rgenerator

--- mainRecord.ps1
+++ mainRecord.ps1
@@ -33,7 +33,7 @@ switch  ($promptRecordingBox) {
     $mouseRecordJob = Start-Job -filePath .\mouseRecord.ps1 -ArgumentList $mouseTrackFrequency
 
     ## RECORDING BOX
-      $recordingBox = [System.Windows.MessageBox]::Show('RECORDING - Click "Ok" when you are done recording','RecordWhatIDo','Ok','Warning')
+      $recordingBox = [System.Windows.MessageBox]::Show('RECORDING - Click "Ok" when you are done recording - Do not enter any sort of credentials during recording','RecordWhatIDo','Ok','Warning')
       switch ($recordingBox) {
 
           'Ok' {
@@ -41,12 +41,16 @@ switch  ($promptRecordingBox) {
               $dirName = "trackFiles"
               New-Item -Path "./" -Name $dirName -ItemType "directory" -Force
 
+              ## STORING GLOBAL INFORAMATIONS
+              $globalInfosFile = New-Item -Path "./$dirName" -Name "globalInfo.txt" -Force # Force to overwrite file
+              [System.Windows.Forms.Screen]::AllScreens | Out-File $globalInfosFile
+
 
               ## RECEIVE FOCUSED APP TRACKING INFO
               $focusedAppTrack += Receive-Job $focusedAppRecordJob
               Stop-Job $focusedAppRecordJob
               Remove-Job $focusedAppRecordJob
-              $focusedAppTrackFile = New-Item -Path "./$dirName" -Name "focusedAppTrack.txt" -Force # -Force for overwrite file
+              $focusedAppTrackFile = New-Item -Path "./$dirName" -Name "focusedAppTrack.txt" -Force # -Force to overwrite file
               $focusedAppTrack | Out-File $focusedAppTrackFile
 
               ## RECEIVE MOUSE TRACKING INFO
